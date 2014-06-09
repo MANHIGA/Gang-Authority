d4a98482de5bc790730cbbe77376b9f9b9eb7ce7
jkn1092 <jkn1092@gmail.com>
2014-06-09 12:23:47 +0200

    Getter Setter Joueur

--- src/model/Joueur.java
+++ src/model/Joueur.java
@@ -6,6 +6,7 @@
 
 package model;
 
+
 /**
  *
  * @author Rems
@@ -20,4 +21,81 @@ public class Joueur {
     private int nbTues;
     private int argent;
     private String nomGang;
+    
+	public int getIdCompte() {
+		return idCompte;
+	}
+	
+	public void setIdCompte(int idCompte) {
+		this.idCompte = idCompte;
+	}
+	
+	public String getPseudo() {
+		return pseudo;
+	}
+	
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getMdp() {
+		return mdp;
+	}
+	
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	
+	public int getPointsAutorite() {
+		return pointsAutorite;
+	}
+	
+	public void setPointsAutorite(int pointsAutorite) {
+		this.pointsAutorite = pointsAutorite;
+	}
+	
+	public int getNbMorts() {
+		return nbMorts;
+	}
+	
+	public void setNbMorts(int nbMorts) {
+		this.nbMorts = nbMorts;
+	}
+	
+	public int getNbTues() {
+		return nbTues;
+	}
+	
+	public void setNbTues(int nbTues) {
+		this.nbTues = nbTues;
+	}
+	
+	public int getArgent() {
+		return argent;
+	}
+	
+	public void setArgent(int argent) {
+		this.argent = argent;
+	}
+	
+	public String getNomGang() {
+		return nomGang;
+	}
+	
+	public void setNomGang(String nomGang) {
+		this.nomGang = nomGang;
+	}
+	
+	public void afficheInfosJoueur(){
+		
+	}
+    
 }
