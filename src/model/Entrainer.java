c71887a2fedb45ad1a54f13a784d525d0a3dc340
jkn1092 <jkn1092@gmail.com>
2014-06-13 23:36:04 +0200

    RecruterTypeSbire

--- src/model/Entrainer.java
+++ src/model/Entrainer.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -15,12 +16,12 @@ import javax.persistence.Table;
 public class Entrainer implements Serializable{
 
 	@Id
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name="entrainer_idCompte")
 	private Joueur joueur;
 	
 	@Id
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name="entrainer_typeSbire")
 	private TypeSbire typeSbire;
 	
@@ -35,14 +36,11 @@ public class Entrainer implements Serializable{
 	
 	public Entrainer(){}
 
-	public Entrainer(Joueur joueur, TypeSbire typeSbire, Integer nbSbire,
-			Integer pointAttaque, Integer pointDefense) {
+	public Entrainer(Joueur joueur, TypeSbire typeSbire) {
 		super();
 		this.joueur = joueur;
 		this.typeSbire = typeSbire;
-		this.nbSbire = nbSbire;
-		this.pointAttaque = pointAttaque;
-		this.pointDefense = pointDefense;
+		this.nbSbire ++;
 	}
 
 	public Joueur getJoueur() {
@@ -66,7 +64,7 @@ public class Entrainer implements Serializable{
 	}
 
 	public void setNbSbire(Integer nbSbire) {
-		this.nbSbire = nbSbire;
+		this.nbSbire += nbSbire;
 	}
 
 	public Integer getPointAttaque() {
