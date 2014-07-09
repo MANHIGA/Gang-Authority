d5c00b3aafaad5fea2a22c228e397bca9b4de744
LoThanel <remy.ballandras@gmail.com>
2014-07-10 01:15:13 +0200

    parenthèses

--- src/model/Joueur.java
+++ src/model/Joueur.java
@@ -381,7 +381,7 @@ public class Joueur {
 			for (Entrainer e : this.getMesSbires()) {
 				if (e.getTypeSbire().getLibelleTypeSbire().equals(m
 						.getMissionTypeSbire().getLibelleTypeSbire())) {
-					e.setNbSbire(new Integer(nbSbiresEnvoyes * -1));
+					e.setNbSbire(new Integer(nbSbiresEnvoyes * (-1)));
 				}
 			}
 	}
